ee1d18b3606128103e56f232c46a630cba44ba13
Starving._.Minsung <kms73033222@gmail.com>
2023-02-21 20:28:51 +0900

    feat : 이미지 추가

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,6 @@
 export { default as LogoImg } from "./logo.png";
+export { default as LoginBGI } from "./loginBackground.png";
+export { default as OpenEye } from "./openEye.svg";
+export { default as CloseEye } from "./closeEye.svg";
+export { default as CheckBox } from "./checkBox.svg";
+export { default as CheckedBox } from "./checkedBox.svg";
